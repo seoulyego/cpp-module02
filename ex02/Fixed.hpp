9b8c3f18a7d2c36c8514edd71dfccc6aef7a7471
yeongo <yeongo@student.42seoul.kr>
2023-07-23 13:31:14 +0900

    add nl

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -14,6 +14,7 @@ class Fixed {
 		Fixed(const Fixed &object);
 		Fixed& operator=(const Fixed& object);
 		~Fixed();
+
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		int		toInt(void) const;
